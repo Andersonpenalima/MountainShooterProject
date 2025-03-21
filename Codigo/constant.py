b1cb825f2472aa7f05c431bd6b273c638732b34e
ANDERSON <andersonpenalima@gmail.com>
2025-03-21 18:14:20 -0300

    MAIN LOOP AND MENU

---
+++ Codigo/constant.py
@@ -0,0 +1,13 @@
+# cores
+COLOR_OY = (253, 116, 4)
+COLOR_W = (255, 255, 255)
+# Menu
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
+               'EXIT')
+
+# altura e largura
+WIN_WIDTH = 576
+WIN_HEIGHT = 324
